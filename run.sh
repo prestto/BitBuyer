6375cdb8e3dc163e767298f2cbb2d74697d40f9f
Tom PRESTON <tom.preston377@gmail.com>
2021-10-06 17:49:24 +0200

    added postgres and seeding script

---
+++ run.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+cecho() {
+    LB='\033[1;36m' # Light Blue
+    LG='\033[1;32m' # Light Green
+    YE='\033[1;33m' # Yellow
+
+    # print normally if no second arg added
+    if [[ $2 == "" ]]; then
+        echo $1
+        return
+    fi
+
+    case $1 in
+    BL)
+        printf "$LB%s\033[0m\n" "$2" # Light Cyan
+        ;;
+    GR)
+        printf "$LG%s\033[0m\n" "$2" # Light green
+        ;;
+    YE)
+        printf "$YE%s\033[0m\n" "$2" # Light green
+        ;;
+    normal | *)
+        echo $2 # Light Purple
+        ;;
+    esac
+}
+
+function run_dev {
+    cecho "BL" "Tilting..."
+    tilt up
+}
+
+function run_seed {
+    cecho "BL" "Seeding..."
+    pipenv run python ./scripts/seed.py
+    cecho "BL" "Seeding complete."
+}
+
+function show_help {
+    cecho "BL" "Help: $0 <ACTION>"
+    cecho "BL" "Parameters :"
+    cecho "BL" " - ACTION values :"
+    cecho "BL" "   * dev                            - Run tilt up."
+    cecho "BL" "   * seed                           - Run db seeding."
+}
+
+if [[ "$1" == "" ]]; then
+    cecho "No arguments provided."
+    show_help
+    exit 1
+fi
+
+case "$1" in
+dev)
+    run_dev
+    ;;
+seed)
+    run_seed
+    ;;
+*)
+    show_help
+    ;;
+esac
---
+++ scripts/seed.py
@@ -0,0 +1,115 @@
+"""
+Create tables for the tweets and prices
+"""
+
+from db import PostgresConnection
+
+
+def create_tables():
+    print('Creating table: twitter_users')
+    query = """
+        create table twitter_users
+        (
+            author integer PRIMARY KEY,
+            name varchar(255),
+            username varchar(255),
+            description varchar(1023)
+        );
+    """
+    with PostgresConnection() as pg:
+        pg.execute(query)
+
+    print('Creating table: tweets')
+    query = """
+        create table tweets
+        (
+            id varchar(31),
+            author_id integer,
+            created_at TIMESTAMPTZ,
+            text_ varchar(511),
+            PRIMARY KEY(id),
+            constraint fk_twitter_users
+                foreign key(author_id)
+                    references twitter_users(author)
+                        on delete cascade
+        );
+    """
+    with PostgresConnection() as pg:
+        pg.execute(query)
+
+    print('Creating table: tweet_sentiment')
+    query = """
+        create table tweet_sentiment
+        (
+            id serial,
+            tweet_id varchar(31),
+            verdict json,
+            source varchar(63),
+            version varchar(63),
+            created_at timestamptz,
+            constraint fk_tweet_id
+                foreign key(tweet_id)
+                    references tweets(id)
+                        on delete cascade
+        );
+    """
+    with PostgresConnection() as pg:
+        pg.execute(query)
+
+    print('Creating table: coins')
+    query = """
+        create table coins
+        (
+            id serial primary key,
+            name varchar(31),
+            abbreviation varchar(7),
+            description varchar(511)
+        );
+    """
+    with PostgresConnection() as pg:
+        pg.execute(query)
+
+    print('Creating table: coin_prices')
+    query = """
+        create table coin_prices
+        (
+        id serial primary key,
+        coin_id integer,
+        rate_close double precision,
+        rate_high double precision,
+        rate_low double precision,
+        rate_open double precision,
+        time_close timestamptz,
+        time_open timestamptz,
+        time_period_end timestamptz,
+        time_period_start timestamptz,
+        constraint fk_coins
+            foreign key(coin_id)
+                references coins(id)
+                    on delete cascade
+        );
+    """
+    with PostgresConnection() as pg:
+        pg.execute(query)
+
+
+def seed_tables():
+    print('Seeding table: twitter_users')
+    query = """
+        insert into twitter_users (author, name, username, description)
+        values (123, 'tom', 'usertom', 'champion')
+    """
+    with PostgresConnection() as pg:
+        pg.execute(query)
+
+
+def main():
+    # create all tables
+    create_tables()
+
+    # seed some dummy data
+    seed_tables()
+
+
+if __name__ == "__main__":
+    main()
