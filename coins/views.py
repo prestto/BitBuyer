70ea0e783c8e8e3577a7fa993db39f55c93f89fb
Tom PRESTON <tom.preston377@gmail.com>
2021-10-11 15:44:49 +0100

    ordering for list view

--- coins/views.py
+++ coins/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import filters, status
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 
@@ -12,3 +12,8 @@ class ListCoins(ListAPIView):
     queryset = Coins.objects.all()
     pagination_class = StandardResultsSetPagination
     serializer_class = CoinSerializer
+    
+    # ordering
+    filter_backends = [filters.OrderingFilter]
+    # ordering_fields = ['id']
+    ordering = ['id']
